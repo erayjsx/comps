95d2262b27dbf3040d9e4cfb8d6598f60012629b
macbook <macbook@192.168.1.23>
2024-07-12 02:02:54 +0300

    Çakışmaları çözdüm

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import "./header-component";
 import "./button-component";
 import "./dropdown-component";
+import "./modal-component";
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 import "./header-component";
 import "./button-component";
 import "./dropdown-component";
+import "./modal-component";
 //# sourceMappingURL=index.js.map
